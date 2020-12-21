cba54c351a4d8def13f37fe925d98c6ccce9c7c9
Ryan Wire <ryanwire@outlook.com>
2020-12-22 00:28:18 +0300

    Sets up deleting a recipe from firestore db

--- js/ui.js
+++ js/ui.js
@@ -19,11 +19,17 @@ const renderRecipe = (data, id) => {
         <div class="recipe-title">${data.title}</div>
         <div class="recipe-ingredients">${data.ingredients}</div>
       </div>
-      <div class="recipe-delete">
+      <div class="recipe-delete pointer">
         <i class="material-icons" data-id="${id}">delete_outline</i>
       </div>
     </div>
   `;
   recipes.innerHTML += html;
 
+};
+
+// remove recipe
+const removeRecipe = (id) => {
+  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+  recipe.remove();
 };
