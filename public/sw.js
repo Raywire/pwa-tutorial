3594f6c1986f9524e575fd10ff0fcef27f53445b
Ryan Wire <ryanwire@outlook.com>
2020-12-22 01:29:31 +0300

    Prevents service worker from trying to cache chrome-extension scheme

--- public/sw.js
+++ public/sw.js
@@ -53,6 +53,9 @@ self.addEventListener('activate', evt => {
 
 // fetch event
 self.addEventListener('fetch', evt => {
+  // check if request is made by chrome extensions or web page
+  // if request is made for web page url must contains http.
+  if (!(evt.request.url.indexOf('http') === 0)) return; // skip the request. if request is not made with http protocol
   if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
     evt.respondWith(
       caches.match(evt.request).then(cacheRes => {
