4f6e0f0288ed432b1d1b02b7e310304b56a6bea1
Ryan Wire <ryanwire@outlook.com>
2020-12-21 23:02:50 +0300

    Sets up realtime data from firestore

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v2';
+const staticCacheName = 'site-static-v3';
 const dynamicCacheName = 'site-dynamic-v1';
 
 const assets = [
@@ -54,21 +54,21 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
   // console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
+  // evt.respondWith(
+  //   caches.match(evt.request).then(cacheRes => {
+  //     return cacheRes || fetch(evt.request).then(fetchRes => {
+  //       return caches.open(dynamicCacheName).then(cache => {
+  //         cache.put(evt.request.url, fetchRes.clone());
 
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15);
-          return fetchRes;
-        })
-      }).catch(() => {
-        if(evt.request.url.indexOf('.html') > -1){
-          return caches.match('/pages/fallback.html');
-        } 
-      })
-    })
-  );
+  //         // check cached items size
+  //         limitCacheSize(dynamicCacheName, 15);
+  //         return fetchRes;
+  //       })
+  //     }).catch(() => {
+  //       if(evt.request.url.indexOf('.html') > -1){
+  //         return caches.match('/pages/fallback.html');
+  //       } 
+  //     })
+  //   })
+  // );
 });
